e1b7d0c080d35c25a8455ad6e839a01c3c6752d2
Carlosguimara <139878571+Carlosguimara@users.noreply.github.com>
2023-07-20 12:40:12 +0000

    exercicio de aula 13 questoes

---
+++ execaula.js
@@ -0,0 +1,18 @@
+var minhaVar;
+minhaVar=10;
+var soma=15+8;
+console.log('minhaVar',minhaVar,'soma=',soma);
+soma= ++soma
+console.log('soma=',soma);
+soma*=3
+console.log('soma=',soma);
+var booleano=true
+console.log('booleano=',booleano);
+var comida=['Arroz','Feijão','Ovo'];
+console.log(comida[1]);
+console.log(soma === minhaVar)
+console.log(minhaVar <= soma)
+console.log(divisao(10,2))
+function divisao(a,b){
+    return a/b
+}
