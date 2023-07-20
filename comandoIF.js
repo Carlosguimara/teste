6aa00fb2e4a93324f12be0300ee1e0421be334b2
Carlosguimara <139878571+Carlosguimara@users.noreply.github.com>
2023-07-20 14:40:31 +0000

    Comando IF ELSE IF

--- comandoIF.js
+++ comandoIF.js
@@ -1,29 +1,27 @@
-//exercicio para treinar IF ELSE
+//exercicio para treinar IF ELSE IF
+//3-Classifique uma variável em 4 categorias: boolean, number, string, undefined
+//4-classifique um numero em 5 categorias: negativo, zero, positiovo, par ou impar
+//5-verifique se um nbumero é divisivel popr 2,3,5,7
 
 
-var n1,texto;
-n1=8
-texto='Julia'
-if (n1 >=0){
+var n1,variavel;
+n1=-5
+variavel=true
+
+//1-Classifique um numero em 3 categorias:negativo, Zero ou Positivo
+if (n1 >0){
     console.log(n1,' É positivo')
-   }   else{
-    console.log(n1, 'É negativo')
-}
-if (n1%2 === 0){
-    console.log(n1,' É par')
-}   else{console.log(n1,' É impar')}
+   }   else if (n1<0){console.log(n1, 'É negativo')}
+       else{ console.log(n1, 'É Zero')
+}      
+             
+//2-Verfifique se o numero é divisivel por 2,3 ou 5 
 
-if (texto ===  ''){
-    console.log(n1,' Não tem conteúdo')
-}   else{console.log('texto= ',texto)}
-if (n1 > 10){
-    console.log(n1,' É maior que 10')
-}   else{ n1 = n1*2;
-    console.log('n1 passou a valer ',n1)
+let n2=300
+if (n2%2 === 0 ){
+    console.log(n1,' É divisivel por 0')}
+if (n2%3 === 0){ 
+    console.log(n1,' É divisivel por 3')}
+if (n2%5 === 0){ 
+        console.log(n1,' É divisivel por 5')
 }
-if (typeof texto === 'boolean')
-{
-    console.log(texto,' É uma variavel Booleana')
-}   else { texto=true;
-    console.log('texto passou a valer ',typeof texto)}
-    
