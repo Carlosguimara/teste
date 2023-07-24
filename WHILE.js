e234e821c695c9c8ad5e5e1c6569b5a3a62df5bb
Carlosguimara <139878571+Carlosguimara@users.noreply.github.com>
2023-07-24 18:03:54 -0300

    Update WHILE.js

--- WHILE.js
+++ WHILE.js
@@ -44,3 +44,68 @@ while (nfinal<=1000) {
   nfinal = nfinal*2
 }
 console.log('número inicial ',ninicial,'| número final', nfinal)
+
+//Verifique se um número é primo.
+var div=2;
+var n=241;
+var cont=0;
+while (div<n && cont==0) {
+  if (n%div==0) {
+    cont= ++cont;    }
+   div= ++div;  
+}
+if (cont !=0){
+  console.log(n,'não é um numero primo')}
+else {
+  console.log(n,'é um número primo')}
+
+//Faça o usuário digitar sua senha até que seja digitada a senha correta.
+
+var senha='senha1234@'
+var digit=window.prompt('Digite sua senha')
+while (digit != senha){
+      digit=window.prompt('Senha não confere !, digite novamente sua senha ')}
+window.alert(' Senha confirmada');
+
+//Imprima os múltiplos de 3 de 1 a 30.
+
+var div=3;
+var liminf=1;
+var limsup=30;
+console.log('Os multiplos de', div,'entre',liminf,'e',limsup,'são:');
+while (liminf<=limsup){
+  if (liminf%div == 0){
+    console.log(liminf);}
+  liminf= ++liminf;
+}
+
+//Calcule a média de uma lista de números
+
+var lista, tamlist, cont, media;
+lista = [10, 20, 60];
+tamlist = lista.length;
+cont = 1;
+media = 0;
+while (cont <= tamlist) {
+  media = media + lista[(cont - 1)];
+  cont = cont + 1;
+}
+window.alert(media / tamlist);
+
+//Calcule o fatorial de um número.
+var fat, cont;
+n=5;
+fat=cont=n;
+while (cont > 1){
+  cont= --cont;
+  fat=fat*(cont-n);
+  }
+console.log(n,'! =',fat);
+
+
+//Imprima os números de 10 a 1 em ordem decrescente.
+var n=10;
+ while (n>0){
+   console.log(n);
+   n= --n
+   
