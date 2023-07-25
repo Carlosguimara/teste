8f17288f15fba656dffc158fae3a06bdff6d24b7
Carlosguimara <139878571+Carlosguimara@users.noreply.github.com>
2023-07-25 14:20:07 +0000

    corrigir

--- blackjack.js
+++ blackjack.js
@@ -1,12 +1,29 @@
 // instalar no node npm install readline-sync para usar question (prompt)
 
-var carta1, carta2, soma;
-carta1=9;
-carta2=13;
-soma=carta1+carta2
+var cj1, cj2, somaj, cb1,cb2,cb3,cb4,somab;
+cj1=9;
+cj2=10;
+somaj=cj1+cj2;
+cb1=3;
+cb2=13;
+cb3=1;
+cb4=7;
+somab=cb1+cb2+cb3+cb4;
+if (somaj>21 || somab>21 ){
+    switch( somaj>21)
+    case true;
+       console.log(`A soma de suas cartas foi ${somaj}, VOCÊ PERDEU !!!`)
+       break;
+    default:
+       console.log(`A soma do banco foi ${somab}, BANCO PERDEU !!!`)}
+elseif (somaj > somab){
+    console.log(`Você ${somaj}, Banco ${somab}, VOCÊ GANHOU !!!`)}
+else{
+    console.log(`Você ${somaj}, Banco ${somab}, BANCO GANHOU !!!`)}
+}
 
-if (soma>21){
-    console.log(`A soma de suas cartas foi ${soma}, VOCÊ PERDEU !!!`)}
-    else{console.log(`A soma de suas cartas foi ${soma}, VOCÊ continua no jogo`)}
+
+
+
 
 
