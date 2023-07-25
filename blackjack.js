4ca0a0f8734d98628387358cf36f2cefb317f4a2
Carlosguimara <139878571+Carlosguimara@users.noreply.github.com>
2023-07-25 13:32:16 +0000

    Blackjack inicial

---
+++ blackjack.js
@@ -0,0 +1,12 @@
+// instalar no node npm install readline-sync para usar prompt
+
+var carta1, carta2, soma;
+carta1=9;
+carta2=13;
+soma=carta1+carta2
+
+if (soma>21){
+    console.log(`A soma de suas cartas foi ${soma}, VOCÊ PERDEU !!!`)}
+    else{console.log(`A soma de suas cartas foi ${soma}, VOCÊ continua no jogo`)}
+
+
