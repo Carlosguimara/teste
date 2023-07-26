8beae258e6e394566c82732abb5f5bc34391bf8b
Carlosguimara <139878571+Carlosguimara@users.noreply.github.com>
2023-07-26 11:43:35 +0000

    disputa com banco ok falta def empate

--- blackjack.js
+++ blackjack.js
@@ -1,28 +1,27 @@
 // instalar no node npm install readline-sync para usar question (prompt)
 
 var cj1, cj2, somaj, cb1,cb2,cb3,cb4,somab;
-cj1=9;
+cj1=10;
 cj2=10;
 somaj=cj1+cj2;
 cb1=3;
 cb2=13;
-cb3=1;
-cb4=7;
+cb3=3;
+cb4=2;
 somab=cb1+cb2+cb3+cb4;
 if (somaj>21 || somab>21 ){
     switch(somaj>21){
-    case true;
+    case true:
        console.log(`A soma de suas cartas foi ${somaj}, VOCÊ PERDEU !!!`)
        break;
     default:
-       console.log(`A soma do banco foi ${somab}, BANCO PERDEU !!!`)
-    }   
-}   
-elseif (somaj > somab){
+       console.log(`Sua soma foi ${somaj} A soma do banco foi ${somab}, BANCO PERDEU !!!`)
+    }  
+} else if (somaj > somab) {
     console.log(`Você ${somaj}, Banco ${somab}, VOCÊ GANHOU !!!`)}
 else{
     console.log(`Você ${somaj}, Banco ${somab}, BANCO GANHOU !!!`)}
-}
+
 
 
 
