ed1f4ed5b6ffeef194babc11a117d15a63f250ce
Carlosguimara <139878571+Carlosguimara@users.noreply.github.com>
2023-08-01 14:30:01 +0000

    a terminar em casa

--- Arrays.js
+++ Arrays.js
@@ -26,3 +26,34 @@ switch (numeros.includes(n)) {
         console.log(`${n} não está incluido no Array`);
 } 
 
+const array1 = [1,2,3,4,5]
+const array2 = [1,2,3,4,5]
+const array3 = array1.concat(array2)
+console.log(array3)
+
+const numeros1 = [1,2,3,4,5,6,7,8,9,10]
+const pares =numeros1.filter(number => number%2 === 0)
+console.log(pares)
+
+const numeros2 = [3,1,4,1,5,9,2,6,5,3,5]
+console.log(numeros2.sort((a,b) => a - b))
+
+
+const frutas1 = ['maçã','banana','laranja','uva','manga']
+var indice = (frutas1.indexOf('laranja'))
+if (indice != -1){
+    console.log(`posição ${indice}`)
+    }
+    else {
+        console.log(`laranja não está na lista`)
+    }
+
+ /*def testaArray (a):
+      var taman 
+        return resp
+
+        usar typeof
+
+
+
+
