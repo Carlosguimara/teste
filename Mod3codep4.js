4bde88a36af3d89f3054142dba308c37d94be4e4
Carlosguimara <139878571+Carlosguimara@users.noreply.github.com>
2023-08-11 15:45:20 +0000

    coloquei comentario com a questão

--- Mod3codep4.js
+++ Mod3codep4.js
@@ -1,3 +1,9 @@
+/*
+Crie uma lista encadeada em que cada elemento representa uma pessoa.
+
+Ela precisa conter informações como nome, idade e referência ao filho dela.
+*/
+
 class Pessoa{
     constructor(Nome, Idade, Filho){
         this.Nome = Nome;
