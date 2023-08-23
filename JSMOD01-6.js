c9b0e870a7524bac3ce454526820d4dbe7aefe21
Carlosguimara <139878571+Carlosguimara@users.noreply.github.com>
2023-08-23 20:27:19 +0000

    em preparação

---
+++ JSMOD01-6.js
@@ -0,0 +1,12 @@
+
+
+const entrada = require("readline-sync")
+
+var nota1=parseFloat(entrada.question(`Digite a 1a nota: `))
+var nota2=parseFloat(entrada.question(`Digite a 2a nota: `))
+var nota3=parseFloat(entrada.question(`Digite a 3a nota: `))
+
+var media= (nota1+nota2+nota3)/3
+
+console.log(nota.toFixed(2))
+
