cd5d3bf31d2a3fb70ec263989eab1f4c0d0e65be
Carlosguimara <139878571+Carlosguimara@users.noreply.github.com>
2023-08-24 13:12:26 +0000

    exercicios em sala de aula

---
+++ EXListaObjetos.js
@@ -0,0 +1,83 @@
+/*
+//1-Como você cria um objeto vazio em JavaScript?
+let pessoa={}
+console.log(pessoa)
+*/
+
+/*
+//2-Como você adiciona a propriedade "nome" com o valor "João" a um objeto chamado "pessoa"?
+
+pessoa.nome='João'
+console.log(pessoa)
+*/
+
+/*
+//3-Como você acessa o valor da propriedade "idade" de um objeto chamado "usuario"?
+
+let usuario= { nome: 'João',
+            idade: 20}
+console.log(usuario.idade)
+*/
+
+/*
+//4-Suponha que você tenha um objeto "produto" com a propriedade "preco" definida como 50. 
+//  Como você altera o valor dessa propriedade para 75?
+
+let produto= { preco: 50}
+console.log(produto.preco)
+produto.preco=75
+console.log(produto.preco)
+*/
+
+/*
+//5-Como você remove a propriedade "email" de um objeto chamado "contato"?
+
+let usuario= { nome: 'João',
+            idade: 20}
+console.log(usuario)
+delete usuario.idade
+console.log(usuario)
+*/
+
+/*
+//6-Como você adiciona uma propriedade "endereco" com o valor "Rua A, 123" a um objeto existente chamado "usuario"?
+
+let usuario= { nome: 'João',
+            idade: 20}
+console.log(usuario)
+usuario.endereco='Rua A, 123'
+console.log(usuario)
+*/
+
+/*
+//7-Se você tem um objeto "empresa" com uma propriedade "departamentos", como você acessa o nome do primeiro departamento?
+
+let empresa={departamentos: ["dep1","dep2","dep3"]}
+console.log(empresa.departamentos[0])
+*/
+
+/*
+//8-Suponha que você tenha um objeto "aluno" com a propriedade "notas" contendo [85, 90, 78]. Como você altera a 
+//  segunda nota para 95?
+
+let aluno={notas: [85,90,78]}
+aluno.notas.splice(1,1,95)
+console.log(aluno)
+*/
+
+/*
+//9-Se você tem um objeto "dados" com uma propriedade "valores" contendo [10, 20, 30], como você remove o segundo 
+//  valor (20) desse array?
+
+let dados= {valores: [10,20,30]}
+dados.valores.splice(1,1)
+console.log(dados)
+*/
+
+/*
+//10-Como você cria um objeto com as propriedades "titulo" (com o valor "Livro") e "autor" (com o valor "Maria")?
+
+let livros= {titulo: 'livro',
+             autor: 'Maria'}
+console.log(livros)
+*/
