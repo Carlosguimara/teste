acea2a7c87e3cfbc79988df408b6b52099502a9f
Carlosguimara <139878571+Carlosguimara@users.noreply.github.com>
2023-08-29 14:03:25 +0000

    fazendo exercicio

--- ExSldM02A01.js
+++ ExSldM02A01.js
@@ -78,3 +78,38 @@ switch (ano%4){
        console.log(`valor da mercadoria - R$${valor} , Impostos(${((aliq-1)*100).toFixed(2)}%) - R$${(valor*(aliq-1)).toFixed(2)}, Valor Total - R$${total.toFixed(2)}`)
    }
    */
+
+ /*4-Faça um programa que calcule e mostre a soma dos N primeiros números pares. 
+     Faça três versões desse programa, cada uma usando uma estrutura de repetição diferente 
+     (for, while, do-while). Qual das três estruturas é a mais adequada para este programa? Por que?
+ 
+
+let soma=0,qtd=50
+for (let i=1; i<=(qtd*2); i++) {
+    if (i%2==0){
+       soma +=i
+    }
+}
+console.log(`a soma dos primeiros ${qtd} numeros pares é ${soma} (calculado com e estrutura FOR)`)
+
+let soma1=0,qtd1=50, i=1
+while  (i <= qtd1*2) {
+    if (i%2==0){
+       soma1 +=i
+    }
+    i++
+}
+console.log(`a soma dos primeiros ${qtd} numeros pares é ${soma} (calculado com e estrutura WHILE)`)
+
+let soma2=0,qtd2=50, i2=1
+do  {
+    if (i2%2==0){
+       soma2 +=i2
+    }
+    i2++
+} while (i2 <= qtd2*2)
+
+console.log(`a soma dos primeiros ${qtd} numeros pares é ${soma} (calculado com e estrutura DO WHILE)`)
+
+*/
+
--- Rascunho.js
+++ Rascunho.js
@@ -1,33 +1,38 @@
 const leitor = require('readline-sync')
 
-/* Uma determinada empresa vende seus produtos em quatro diferentes estados. Cada estado possui uma taxação de 
-   imposto diferente sobre o produto (MG: 7%; SP: 12%; RJ: 15%; MS: 8%). Faça um programa em Javascript, que o 
-   usuário entre com o valor e o estado destino do produto e o programa retorne o preço final do produto acrescido 
-   do imposto correspondente ao estado onde será vendido. O programa deve mostrar uma mensagem de erro caso o estado 
-   fornecido não esteja na lista de estados válidos. */
+/*4-Faça um programa que calcule e mostre a soma dos N primeiros números pares. 
+    Faça três versões desse programa, cada uma usando uma estrutura de repetição diferente 
+    (for, while, do-while). Qual das três estruturas é a mais adequada para este programa? Por que?
+ */
 
-let aliq=0.00
-let valor = parseFloat(leitor.question('Digite o valor da mercadoria: R$')).toFixed(2)
-let estado= leitor.question('DIgite o Estado de destino da mercadoria (MG,SP,RJ,MS): ').toUpperCase()
-
-    switch (estado){
-
-        case 'MG':
-            aliq=1.07
-            break
-        case 'SP':
-            aliq=1.12  
-            break
-        case 'RJ':
-            aliq=1.15  
-            break 
-        case 'MS':
-            aliq=1.08
-            break
-        default:
-            console.log(`Estado não cadastrado - ${estado}`)
+let soma=0,qtd=50
+for (let i=1; i<=(qtd*2); i++) {
+    if (i%2==0){
+       soma +=i
     }
-if (aliq > 0) {
-    let total= valor*aliq
-    console.log(`valor da mercadoria - R$${valor} , Impostos(${((aliq-1)*100).toFixed(2)}%) - R$${(valor*(aliq-1)).toFixed(2)}, Valor Total - R$${total.toFixed(2)}`)
 }
+console.log(`a soma dos primeiros ${qtd} numeros pares é ${soma} (calculado com e estrutura FOR)`)
+
+let soma1=0,qtd1=50, i=1
+while  (i <= qtd1*2) {
+    if (i%2==0){
+       soma1 +=i
+    }
+    i++
+}
+console.log(`a soma dos primeiros ${qtd} numeros pares é ${soma} (calculado com e estrutura WHILE)`)
+
+let soma2=0,qtd2=50, i2=1
+do  {
+    if (i2%2==0){
+       soma2 +=i2
+    }
+    i2++
+} while (i2 <= qtd2*2)
+
+console.log(`a soma dos primeiros ${qtd} numeros pares é ${soma} (calculado com e estrutura DO WHILE)`)
+
+
+
+
+    
