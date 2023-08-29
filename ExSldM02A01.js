5769d786feef202f4fc7f9b5a2af29b63be6ed82
Carlosguimara <139878571+Carlosguimara@users.noreply.github.com>
2023-08-29 14:11:07 +0000

    em execução

--- ExSldM02A01.js
+++ ExSldM02A01.js
@@ -113,3 +113,15 @@ console.log(`a soma dos primeiros ${qtd} numeros pares é ${soma} (calculado com
 
 */
 
+/*5-Faça um programa em Javascript que receba do usuário um número N e depois imprima os N primeiros 
+    números naturais ímpares.
+
+    let qtd= parseInt(leitor.question(`Digite a qtde desejada `))   
+
+    let soma=0
+    for (let i=1; i<=(qtd*2); i++) {
+        if (i%2!==0){
+           console.log(i)
+        }
+    }
+*/
--- Rascunho.js
+++ Rascunho.js
@@ -1,36 +1,18 @@
 const leitor = require('readline-sync')
 
-/*4-Faça um programa que calcule e mostre a soma dos N primeiros números pares. 
-    Faça três versões desse programa, cada uma usando uma estrutura de repetição diferente 
-    (for, while, do-while). Qual das três estruturas é a mais adequada para este programa? Por que?
- */
+/*5-Faça um programa em Javascript que receba do usuário um número N e depois imprima os N primeiros 
+    números naturais ímpares.*/
 
-let soma=0,qtd=50
+let qtd= parseInt(leitor.question(`Digite a qtde desejada `))   
+
+let soma=0
 for (let i=1; i<=(qtd*2); i++) {
-    if (i%2==0){
-       soma +=i
+    if (i%2!==0){
+       console.log(i)
     }
 }
-console.log(`a soma dos primeiros ${qtd} numeros pares é ${soma} (calculado com e estrutura FOR)`)
 
-let soma1=0,qtd1=50, i=1
-while  (i <= qtd1*2) {
-    if (i%2==0){
-       soma1 +=i
-    }
-    i++
-}
-console.log(`a soma dos primeiros ${qtd} numeros pares é ${soma} (calculado com e estrutura WHILE)`)
 
-let soma2=0,qtd2=50, i2=1
-do  {
-    if (i2%2==0){
-       soma2 +=i2
-    }
-    i2++
-} while (i2 <= qtd2*2)
-
-console.log(`a soma dos primeiros ${qtd} numeros pares é ${soma} (calculado com e estrutura DO WHILE)`)
 
 
 
