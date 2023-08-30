2adfd8ee8a604a49f23b173db4711684d839325d
Carlosguimara <139878571+Carlosguimara@users.noreply.github.com>
2023-08-30 15:27:38 -0300

    Update JSMOD03-3.js

--- JSMOD03-3.js
+++ JSMOD03-3.js
@@ -20,7 +20,33 @@ console.log(comparametroeretorno(param))
 let prof='Waldeck'
 console.log(texto(prof))
 
-//***** Parte II ***** exemplo de programa utilizando os 3 tipos de função
+/***** Parte II ***** exemplo de programa utilizando os 3 tipos de função
+                    Poderia ter sido feito de uma foma mais otimizada, porém
+                    o objetivo é mostrar o uso dos 3 tipos de função *******/
+
+const readline = require("readline-sync")
+
+let operacao= readline.question('Digite o Tipo de Operação (+ ,- ,/ , *) - ')
+let num01 = parseInt(readline.question('Digite o 1° número - '))
+let num02 = parseInt(readline.question('Digite o 2° número - ')) 
+
+//Função sem parametro e retorno   
+function agradecimento(){
+    console.log(`Obrigado por me ajudar a testar funções !!!`)
+}
+
+//Função Arrow
+const calc=(oper,[n1,n2])=> eval(`${n1}${oper}${n2}`)
+
+//Função com parametro e retorno
+function resposta(cont){
+    msg=`${num01}${operacao}${num02}=${cont}`
+    return msg;
+  }
+agradecimento()
+let conta=calc(operacao,[num01,num02])
+console.log(resposta(conta))
+ 
 
 
 
