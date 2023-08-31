4b3569b0aaa5a3c248836168173afd6f4e797379
Carlosguimara <139878571+Carlosguimara@users.noreply.github.com>
2023-08-31 10:53:57 -0300

    Update Rascunho.js

--- Rascunho.js
+++ Rascunho.js
@@ -1,7 +1,9 @@
 const leitor = require('readline-sync')
 
-/*8-Uma empresa paga R$ 10,00 por hora normal trabalhada e R$ 15,00 por hora extra. Escreva um programa que leia o total de horas normais 
-e o total de horas extras trabalhadas por um empregado em um ano e calcule o salário anual deste trabalhador.
+/*9-Elabore um programa em JS para que leia três números e mostre-os em ordem crescente.
+
+do {
+  
 
 let horasnormais= parseInt(leitor.question(`Digite o numero de Horas Normais trabalhadas no ano `))   
 let horasextras= parseInt(leitor.question(`Digite o numero de Horas Extras trabalhadas no ano `)) 
