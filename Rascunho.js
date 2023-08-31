8f49b1c8b30437b1001927148b34762f751d0f2e
Carlosguimara <139878571+Carlosguimara@users.noreply.github.com>
2023-08-31 10:48:23 -0300

    Update Rascunho.js

--- Rascunho.js
+++ Rascunho.js
@@ -3,20 +3,11 @@ const leitor = require('readline-sync')
 /*8-Uma empresa paga R$ 10,00 por hora normal trabalhada e R$ 15,00 por hora extra. Escreva um programa que leia o total de horas normais 
 e o total de horas extras trabalhadas por um empregado em um ano e calcule o salário anual deste trabalhador.
 
-let inicio= leitor.question(`Digite a hora de inicio no formato hh:mm:ss - `)
-let horain= inicio.split(":")
-//console.log(horain)
+let horasnormais= parseInt(leitor.question(`Digite o numero de Horas Normais trabalhadas no ano `))   
+let horasextras= parseInt(leitor.question(`Digite o numero de Horas Extras trabalhadas no ano `)) 
+let valorN=horasnormais*10.00
+let valorE=horasextras*15.00
+let valoranual=valorN+valorE
 
-let dura= parseInt(leitor.question(`Digite a duração da experiencia em segundos - `))
+console.log(`Salario Anual=${valoranual}`)
 
-let inicseg = horain[0]*3600 + horain[1]*60 + horain[2]*1
-//console.log( horain[0]*3600 , horain[1]*60 , horain[2], inicseg)
-let fimseg = inicseg+dura
-console.log(inicseg,fimseg)
-
-let hfim=Math.trunc(fimseg/3600)
-let mfim=Math.trunc((fimseg/3600-hfim)*60)
-let sfim=((((fimseg/3600-hfim)*60)-mfim)*60).toFixed(0)
-
-let fim =(`${hfim}:${mfim}:${sfim}`)
-console.log(fim) 
