8b94e1a100b9b1466823d787f9879cfdc23d69e3
Carlosguimara <139878571+Carlosguimara@users.noreply.github.com>
2023-08-31 11:18:38 -0300

    Update ExSldM02A01.js

--- ExSldM02A01.js
+++ ExSldM02A01.js
@@ -173,3 +173,18 @@ let valoranual=valorN+valorE
 
 console.log(`Salario Anual=R$${valoranual.toFixed(2)}`)
 */
+
+/*9-Elabore um programa em JS para que leia três números e mostre-os em ordem crescente.
+
+do {
+  
+
+let horasnormais= parseInt(leitor.question(`Digite o numero de Horas Normais trabalhadas no ano `))   
+let horasextras= parseInt(leitor.question(`Digite o numero de Horas Extras trabalhadas no ano `)) 
+let valorN=horasnormais*10.00
+let valorE=horasextras*15.00
+let valoranual=valorN+valorE
+
+console.log(`Salario Anual=${valoranual}`)
+*/
+
