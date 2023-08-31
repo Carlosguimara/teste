4f81faeff0727b5bf33d9dac60f430033f5311ec
Carlosguimara <139878571+Carlosguimara@users.noreply.github.com>
2023-08-31 10:29:29 -0300

    Update ExSldM02A01.js

--- ExSldM02A01.js
+++ ExSldM02A01.js
@@ -139,3 +139,25 @@ console.log(`Valor da Diaria R$${valor.toFixed(2)}, Dias trabalhados ${dias}, Va
 
 */
 
+/*7-Faça um programa para ler um horário (hora:minuto:segundo) de início e a duração, em segundos, de uma experiência 
+    biológica. O programa deve informar o horário (hora:minuto:segundo) de término da mesma.
+
+let inicio= leitor.question(`Digite a hora de inicio no formato hh:mm:ss - `)
+let horain= inicio.split(":")
+//console.log(horain)
+
+let dura= parseInt(leitor.question(`Digite a duração da experiencia em segundos - `))
+
+let inicseg = horain[0]*3600 + horain[1]*60 + horain[2]*1
+//console.log( horain[0]*3600 , horain[1]*60 , horain[2], inicseg)
+let fimseg = inicseg+dura
+console.log(inicseg,fimseg)
+
+let hfim=Math.trunc(fimseg/3600)
+let mfim=Math.trunc((fimseg/3600-hfim)*60)
+let sfim=((((fimseg/3600-hfim)*60)-mfim)*60).toFixed(0)
+
+let fim =(`${hfim}:${mfim}:${sfim}`)
+console.log(fim) 
+*/
+
