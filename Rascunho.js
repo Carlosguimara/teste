b4200d33726af30349c9eba8b53d13963037ea9a
Carlosguimara <139878571+Carlosguimara@users.noreply.github.com>
2023-09-16 12:44:14 +0000

    construindo

--- Rascunho.js
+++ Rascunho.js
@@ -1,62 +1,73 @@
+const readline = require("readline-sync")
+const clear = require('clear')
+let bancoDeEmprestimos = []
+let sequencial=0
 
-let continuar = true
-// MENU INICIAL
-while (continuar) {
-	console.log('\n*** REGISTRO DE EMPRESTIMO DE INSTRUMENTO ***');
-	console.log('\nOPÇÃO:');
-	console.log('1. Emprestar Instrumento');
-	console.log('2. Devolver Instrumento');
-	console.log('3. Listar Emprestimo');
-	console.log('0. Sair');
-	console.log('**********************************************');
-	const opcao = readline.questionInt('Escolha uma opção: ');
+class emprestimo {
+    constructor (sequencial,idInstrumento,idAluno,evento,dataEmprestimo) {
+        this.idEmprestimo=
+        this.idInstrumento
+        this.idAluno
+        this.evento
+        this.dataEmprestimo
+        this.dataDevolucao = null
+    }
+}
 
-	switch (opcao) {
-		case 1:
-			const novoemprestimo = new emprestimo(sequencial);
-            if (novoemprestimo.idInstrumento=="*"){
-                console.log(`Empréstimo não efetuado`)
+function EmpEmprestar(IdEmprestimoAnt){
+    let status="ok"
+    while (true) {
+        let idInstrumento = readline.questionInt(`Digite o numero do Instrumento, 0 para desistir: `)
+        if (idInstrumento==0){
+            console.log(`desistindo`)
+            status="d"
+            break
+        }
+    
+        /*buscar no banco de instrumentos
+            if !existir{
+            console.log(`Instrumento inexistente`)
+            continue
+        }else {
+            console.log(`tipo: ${bd.tipo}`)
+            console.log(`descrição: ${bd.descricao}`)
+        } */
+        
+        let idAluno = readline.questionInt(`digite a matricula do aluno: `)
+      /*buscar no banco de instrumentos
+        if !existir{
+            console.log(`Aluno inexistente`)
+            continue
+        }else {
+            console.log(`Aluno: ` ${bd.nome})
+        } */
+
+        let evento = readline.question(`informe onde será usado `)
+        let dataEmprestimo = new Date()
+        let idEmprestimo = idEmprestimoAnterior+1
+        console.log(`Instrumento liberado para empréstimo em ${dataEmprestimo}`)
+        console.log(`Numero sequencial do empréstimo; ${idEmprestimo}`)
+        break
+    }
+    return status, idEmprestimo, idInstrumento, idAluno, evento, dataEmprestimo
+}
+
+function EmpDevolver(idEmprestimo){
+    let devolver = readline.questionInt(`Digite o numero do empréstimo: `)
+    for (i=bancoDeEmprestimos.length-1; i>= -1; --i){
+        if (i < 0){
+            console.log(`Numero de empréstimo inválido`)
+            break
+        }
+        if (bancoDeEmprestimos[i].idEmprestimo==devolver){
+            if (bancoDeEmprestimos[i].dataDevolucao==null){
+                bancoDeEmprestimos[i].dataDevolucao= new Date()
+                console.log(`Instrumento devolvido em ${bancoDeEmprestimos[i].dataDevolucao}`)        
             }else {
-                bancoDeEmprestimos.push(novoemprestimo)
-                sequencial=novoemprestimo.NumeroEmprestimo
-                }
-		case 2:
-            let devolver = readline.questionInt(`Digite o numero do empréstimo: `)
-            for (i=bancoDeEmprestimos.length-1; i>-2; i--){
-                if (i=-1){
-                    console.log(`Numero de empréstimo inválido`)
-                    break
-                }
-                if (bancoDeEmprestimos[i].NumeroEmprestimo==devolver){
-                    if (bancoDeEmprestimos[i].datadev==0){
-                        bancoDeEmprestimos[i].datadev= new Date()
-                    }else {
-                        console.log(`Instrumento já devolvido em ${bancoDeEmprestimos[i].datadev} pelo aluno ${bancoDeEmprestimos[i].aluno}`)
-                    }
-                    break
-                }
+                console.log(`Instrumento já devolvido em ${bancoDeEmprestimos[i].dataDevolucao} pelo aluno ${bancoDeEmprestimos[i].idAluno}`)
             }
-
-		
-		case 3:
-            let emprest = readline.questionInt(`Digite o numero do empréstimo: `)
-            for (i=bancoDeEmprestimos.length-1; i>-2; i--){
-                if (i=-1){
-                    console.log(`Numero de empréstimo inválido`)
-                    break
-                }
-                if (bancoDeEmprestimos[i].NumeroEmprestimo==emprest){
-                    console.log(bancoDeEmprestimos[i])
-                    }
-                }
-		case 0:
-			console.log(`Encerrando o programa`);
-            continuar = false
-			break;
-		
-		default:
-			console.log('\nOpção inválida. Por favor, escolha uma opção válida.');
-			break;
-	}
-};
-
+            break
+        }
+    }
+    continue
+}
